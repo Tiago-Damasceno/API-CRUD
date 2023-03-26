82d050cc73fd8e01267f6bf5665c73ac691cb84e
Tiago Damasceno <devtiago01@gmail.com>
2023-03-25 23:29:10 -0300

    atratribuindo paginas

--- API.py
+++ API.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('__name__')
 
+@app.route('/')
+def homepage(name = None):
+    return render_template("homepage.html", name = name)
 
-print('ta funcionando o flask')
+@app.route('/contatos/<contatos>')
+def contatos(name = None):
+      return render_template("contatos.html", name = name)
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+        app.run(debug=True)
