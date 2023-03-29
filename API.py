99b0d36b519d81c767fab0af25e41298f8ab63e5
Tiago Damasceno <devtiago01@gmail.com>
2023-03-28 23:58:40 -0300

    add navbar

--- API.py
+++ API.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask('__name__')
 
+lista_usuarios = ['Paula', 'Felipe', 'Eloá', 'Raví', 'Tiago']
+
 @app.route('/')
 def homepage(name = None):
     return render_template("homepage.html", name = name)
@@ -10,7 +12,9 @@ def homepage(name = None):
 def contatos(name = None):
       return render_template("contatos.html", name = name)
 
-
+@app.route('/usuarios')
+def usarios(name = None):
+    return render_template("usuarios.html", name = name, lista_usuarios = lista_usuarios)
 
 
 if __name__ == '__main__':
