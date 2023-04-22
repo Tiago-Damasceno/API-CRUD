48d5696956e598863a269b5b2bf13c2de9023953
Tiago Damasceno <devtiago01@gmail.com>
2023-04-21 23:12:59 -0300

    mrp7

--- API.py
+++ API.py
@@ -6,39 +6,40 @@ app = Flask('__name__')
 
 lista_usuarios = ['Paula', 'Felipe', 'Eloá', 'Raví', 'Tiago']
 
-app.secret_key = os.environ.get('FLASK_SECRET_KEY','86a80337ce81d80a3503f02eb197c8c5')
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', '86a80337ce81d80a3503f02eb197c8c5')
 
 
 @app.route('/')
-def homepage(name = None):
-    return render_template("homepage.html", name = name)
+def homepage(name=None):
+    return render_template("homepage.html", name=name)
+
 
 @app.route('/contatos')
-def contatos(name = None):
-      return render_template("contatos.html", name = name)
+def contatos(name=None):
+    return render_template("contatos.html", name=name)
+
 
 @app.route('/usuarios')
-def usuarios(name = None):
-    return render_template("usuarios.html", name = name, lista_usuarios = lista_usuarios)
+def usuarios(name=None):
+    return render_template("usuarios.html", name=name, lista_usuarios=lista_usuarios)
+
 
 @app.route('/login', methods=['GET', 'POST'])
-def login(name = None):
-
+def login(name=None):
     form_login = FormLogin()
     form_criar_conta = FormCriarConta()
 
-
     if form_login.validate_on_submit() and 'botão_login' in request.form:
-        flash(f'login feito com sucesso para o email {form_login.email.data}')
+        flash(f'login feito com sucesso para o email {form_login.email.data}', 'alert-success')
         return redirect(url_for('homepage'))
-        #fez login  com sucesso
+        # fez login  com sucesso
     if form_criar_conta.validate_on_submit() and 'botão_submit_criarconta' in request.form:
-        flash(f'conta criada com sucesso para o email {form_criar_conta.email.data}')
+        flash(f'conta criada com sucesso para o email {form_criar_conta.email.data}', 'alert-success')
         return redirect(url_for('homepage'))
-        #criou conta com sucesso
+        # criou conta com sucesso
 
-    return render_template("login.html", name = name, form_login = form_login, form_criar_conta = form_criar_conta)
+    return render_template("login.html", name=name, form_login=form_login, form_criar_conta=form_criar_conta)
 
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
