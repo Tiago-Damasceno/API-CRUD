1672e104ebca24b3028d4a915b49150f12337165
Tiago Damasceno <devtiago01@gmail.com>
2023-04-22 23:19:16 -0300

    criando db

--- crud.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-dados_conexao = "Driver={SQL Server};Server=DESKTOP-L79M8K7\SQLEXPRESS;Database=bd_PDV;"
-conexão = pyodbc.connect(dados_conexao)
-print("Conexão bem sucedida")
-
-# Criando um cursor para executar as consultas SQL
-cursor = conexão.cursor()
